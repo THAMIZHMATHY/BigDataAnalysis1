c89234edec08575383689decc32acf952eba31f1
THAMIZHMATHY <146329267+THAMIZHMATHY@users.noreply.github.com>
2023-11-01 19:30:46 +0530

    sentimental analysis.py

---
+++ sentimental analysis.py
@@ -0,0 +1,5 @@
+import nltk from nltk.sentiment
+import SentimentIntensityAnalyzer nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer() text = "The weather is wonderful and the scenery is breathtaking."
+sentiment_score = sia.polarity_scores(text)
+print(sentiment_score)
